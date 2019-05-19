da1eccbb538a6d509617f2381d38c3b9e35a85a9
momosh <m.d.miladinovic@gmail.com>
2019-05-19 19:34:44 +0200

    Added app scafold

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Container, Menu, Segment } from 'semantic-ui-react'
+
+import { Tasks } from './components'
+
+const StyledContainer = styled(Container)`
+	margin-top: 25px;
+`
 
 function App() {
-	return <div>SBG</div>
+	return (
+		<StyledContainer>
+			<Menu pointing>
+				<Menu.Item name="projects" />
+				<Menu.Item name="data" />
+				<Menu.Item name="Public Apps" />
+				<Menu.Menu position="right">
+					<Menu.Item>Momosh</Menu.Item>
+				</Menu.Menu>
+			</Menu>
+
+			<Segment>
+				<Tasks />
+			</Segment>
+		</StyledContainer>
+	)
 }
 
 export default App
---
+++ src/components/Tasks.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Tasks = props => <div>Tasks</div>
+
+export default Tasks
