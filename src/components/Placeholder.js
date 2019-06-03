ab2ecb73ddb9f5799c93463bc6fc62f7cf95ea0d
momosh <m.d.miladinovic@gmail.com>
2019-06-03 18:25:45 +0200

    Added new loader component.

---
+++ src/components/Placeholder.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Placeholder as Holder } from 'semantic-ui-react'
+
+const Placeholder = () => (
+	<Holder>
+		<Holder.Header>
+			<Holder.Line />
+			<Holder.Line />
+		</Holder.Header>
+		<Holder.Paragraph>
+			<Holder.Line />
+			<Holder.Line />
+			<Holder.Line />
+			<Holder.Line />
+		</Holder.Paragraph>
+	</Holder>
+)
+
+export default Placeholder
